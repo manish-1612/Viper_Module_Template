e2b5f54ce69d0b61ab397bad09ed564caa216dfa
Manish Kumar <manish.kumar@innofied.com>
2018-01-08 16:43:09 +0530

    Module template added

---
+++ Module VIPER.xctemplate/DataManager/Local/___FILEBASENAME___LocalDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+class ___VARIABLE_viperModuleName___LocalDataManager: ___VARIABLE_viperModuleName___LocalDataManagerInputProtocol {
+
+    init() {}
+    
+    // Data fetch service methods goes here
+
+}
